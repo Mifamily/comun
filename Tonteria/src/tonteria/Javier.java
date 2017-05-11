1eb9101124f14c8a508b6bcbd00426b19ba05b82
Javier <Javier@JAVI-PC>
2017-05-12 00:38:22 +0200

    añado la clase javier

---
+++ Tonteria/src/tonteria/Javier.java
@@ -0,0 +1,19 @@
+package tonteria;
+
+public class Javier {
+	private String Nombre="Javier";
+
+	public Javier(String nombre) {
+		super();
+		Nombre = nombre;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+}
